8f3d0231bf1d177d20ea11356ddd8700b9edce49
Roman Bartusiak <riomus@gmail.com>
2016-09-05 19:21:17 +0200

    Project structure update. Adding sample application.

---
+++ sample/src/main/java/com/a8thlab/sdksample/TrackerSDKTest.java
@@ -0,0 +1,13 @@
+package com.a8thlab.sdksample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TrackerSDKTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tracker_sdktest);
+    }
+}
